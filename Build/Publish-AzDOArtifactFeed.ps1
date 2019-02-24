0603d5b3153d1de219396d548171691f1b583a1e
Adam Rush <adamrushuk@gmail.com>
2019-02-24 18:16:35 +0000

    Added initial Publish-AzDOArtifactFeed.ps1 script

---
+++ Build/Publish-AzDOArtifactFeed.ps1
@@ -0,0 +1,72 @@
+# Variables
+$powershellGetVersion = '1.5.0.0' # DO NOT use the latest 1.6.0 version as there is issues with this process
+# $moduleFolderPath = 'C:\Users\adamr\code\PowerShellPipeline\Staging\PSvCloud' # only target folder, NOT the .psm1 or .psd1
+$moduleFolderPath = "$(System.ArtifactsDirectory)\PowerShellPipeline\Artifacts\PSvCloud"
+$repositoryName = 'psmodules'
+$feedUsername = 'NotChecked'
+$PAT = $env:ArtifactFeedPat
+$packageSourceUrl = "https://adamrushuk.pkgs.visualstudio.com/_packaging/$repositoryName/nuget/v2" # Enter your VSTS AccountName (note: v2 Feed)
+
+# This is downloaded during Step 3, but could also be "C:\Users\USERNAME\AppData\Local\Microsoft\Windows\PowerShell\PowerShellGet\NuGet.exe"
+# if not running script as Administrator.
+$nugetPath = 'C:\ProgramData\Microsoft\Windows\PowerShell\PowerShellGet\NuGet.exe'
+
+# Create credential
+$password = ConvertTo-SecureString -String $PAT -AsPlainText -Force
+$credential = New-Object System.Management.Automation.PSCredential ($feedUsername, $password)
+
+
+# Step 1
+# Upgrade PowerShellGet
+# Install-Module PowerShellGet -RequiredVersion $powershellGetVersion -Force
+# Remove-Module PowerShellGet -Force
+# Import-Module PowerShellGet -RequiredVersion $powershellGetVersion -Force
+
+
+# Step 2
+# Check NuGet is listed
+Get-PackageProvider -Name 'NuGet' -ForceBootstrap
+
+
+# Step 3
+# THIS WILL FAIL first time, so don't panic!
+# Try to Publish a PowerShell module - this will prompt and download NuGet.exe, and fail publishing the module (we publish at the end)
+$publishParams = @{
+    Path        = $moduleFolderPath
+    Repository  = $repositoryName
+    NugetApiKey = 'VSTS'
+    Force       = $true
+    Verbose     = $true
+    ErrorAction = 'SilentlyContinue'
+}
+Publish-Module @publishParams
+
+
+# Step 4
+# Register NuGet Package Source
+& $nugetPath Sources Add -Name $repositoryName -Source $packageSourceUrl -Username $feedUsername -Password $PAT
+
+# Check new NuGet Source is registered
+& $nugetPath Sources List
+
+
+# Step 5
+# Register feed
+$registerParams = @{
+    Name                      = $repositoryName
+    SourceLocation            = $packageSourceUrl
+    PublishLocation           = $packageSourceUrl
+    InstallationPolicy        = 'Trusted'
+    PackageManagementProvider = 'Nuget'
+    Credential                = $credential
+    Verbose                   = $true
+}
+Register-PSRepository @registerParams
+
+# Check new PowerShell Repository is registered
+Get-PSRepository -Name $repositoryName
+
+
+# Step 6
+# Publish PowerShell module (2nd time lucky!)
+Publish-Module @publishParams
